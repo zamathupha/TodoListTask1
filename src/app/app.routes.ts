1338b8e4eb20759d7ac6ede7e2f295030212d58b
Zama Thupha <zamathupha@gmail.com>
2024-11-12 16:44:44 +0200

    guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,25 +3,30 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { LayoutComponent } from './pages/layout/layout.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AuthGuard } from './auth.guard';
 
 export const routes: Routes = [
-    {
-       path: `` ,
-       redirectTo: `logon`,
-       pathMatch: `full`
-    },
-   {
-       path:`login`,
-       component:LoginComponent
-   },
-   {
-      path: ``,
-      component:LayoutComponent,
-      children: [
-        {
-            path: `dashboard`,
-            component:DashboardComponent
-        }
-      ]
-   }
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+        canActivate: [AuthGuard]
+      },
+      
+      
+    ]
+  }
 ];
+
